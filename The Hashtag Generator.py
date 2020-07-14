89feb838189f22a1d764544b222f1ff4f5be9897
LiamCSDev <57903273+LiamCSDev@users.noreply.github.com>
2020-07-14 10:55:33 +0100

    Create The Hashtag Generator.py
    https://www.codewars.com/kata/52449b062fb80683ec000024/python

---
+++ The Hashtag Generator.py
@@ -0,0 +1,20 @@
+# First letter must be a hashtag
+# All word must have their first letter capitalized
+# If the final result is longer than 140 chars it must return false
+# If the input is empty, return false
+def generate_hashtag(s):
+    if (len(s) > 140 or len(s) == 0): 
+        return False
+    else:
+        capitalize = s.title()
+        return '#' + s.title().replace(" ", "")
+
+print(generate_hashtag('')==False)
+print(generate_hashtag('Codewars')=='#Codewars')
+print(generate_hashtag('Codewars      ')=='#Codewars')
+print(generate_hashtag('Codewars Is Nice')=='#CodewarsIsNice')
+print(generate_hashtag('codewars is nice')=='#CodewarsIsNice')
+print(generate_hashtag('CodeWars is nice')=='#CodewarsIsNice')
+print(generate_hashtag('c i n')=='#CIN')
+print(generate_hashtag('codewars  is  nice') =='#CodewarsIsNice')
+print(generate_hashtag('Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat') == False)
